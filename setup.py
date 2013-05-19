248e71873c19e18e683292e104a8df613c79389a
Daniel Bader <mail@dbader.org>
2013-05-19 22:45:14 +0200

    Fix pypi download link pointing to old release... :-)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='schedule',
     packages=['schedule'],
-    version='0.1.2',
+    version='0.1.3',
     description='Job scheduling for humans.',
     author='Daniel Bader',
     author_email='mail@dbader.org',
     url='https://github.com/dbader/schedule',
-    download_url='https://github.com/dbader/schedule/tarball/0.1.1',
+    download_url='https://github.com/dbader/schedule/tarball/0.1.3',
     keywords=['schedule', 'periodic', 'jobs', 'scheduling', 'clockwork'],
     classifiers=[],
 )
