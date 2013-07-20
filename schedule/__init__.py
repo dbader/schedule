c5589ee01f3603f1c87b46cf0fa3ce26af2191a4
Matthew Hardwick <MatthewRHardwick@gmail.com>
2013-07-20 06:08:48 -0500

    Added a conditional check for exceptional case in next_run() when the self.jobs queue is empty. No longer throws a ValueError exception, but rather returns a None object when no jobs are available.

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -111,6 +111,10 @@ class Scheduler(object):
     @property
     def next_run(self):
         """Datetime when the next job should run."""
+        if self.jobs.__len__() > 0:
+            date_time = min(self.jobs).next_run
+        else:
+            return None
         return min(self.jobs).next_run
 
     @property
