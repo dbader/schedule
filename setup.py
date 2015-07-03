550cc733f01c604663f00f1c9b6bd42ac207e01a
Daniel Bader <mail@dbader.org>
2015-07-02 17:58:11 -0700

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
     sys.exit()
 
-SCHEDULE_VERSION = '0.3.1'
+SCHEDULE_VERSION = '0.3.2'
 SCHEDULE_DOWNLOAD_URL = (
     'https://github.com/dbader/schedule/tarball/' + SCHEDULE_VERSION
 )
