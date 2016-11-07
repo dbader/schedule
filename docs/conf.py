2e6095006e115809cb653bb967f595c36654747c
Daniel Bader <mail@dbader.org>
2016-11-07 16:33:56 +0100

    Hopefully fix the docs build on CI

--- docs/conf.py
+++ docs/conf.py
@@ -188,7 +188,9 @@ html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 #
-# html_sidebars = {}
+html_sidebars = {
+    'index':    ['sidebarintro.html', 'sourcelink.html', 'searchbox.html'],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
