c72d930053b21e7e67b9553f15090ee370b0d196
Daniel Bader <mail@dbader.org>
2017-06-09 17:56:54 -0700

    Bump dev dependencies to latest

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Natural Language :: English',
     ],
 )
