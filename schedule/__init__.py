4086f79793ab468143a414c884842efc534ba3df
f3n1xx <arkspeed@gmail.com>
2018-06-19 00:18:19 +0530

    One

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -22,11 +22,11 @@ Usage:
     >>> import time
 
     >>> def job(message='stuff'):
-    >>>     print("I'm working on:", message)
+    >>>     print("I'm working on something:", message)
 
     >>> schedule.every(10).minutes.do(job)
     >>> schedule.every(5).to(10).days.do(job)
-    >>> schedule.every().hour.do(job, message='things')
+    >>> schedule.every().hour.do(job, message='better')
     >>> schedule.every().day.at("10:30").do(job)
 
     >>> while True:
