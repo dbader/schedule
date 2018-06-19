3b6b0b83a98e4ef2867c1d3fb50b361cb6ba688a
f3n1xx <arkspeed@gmail.com>
2018-06-19 05:35:33 +0530

    Contents identical to successful travis test and still failing.

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -120,7 +120,7 @@ class Scheduler(object):
     def every(self, interval=1, till=None):
         """
         Schedule a new periodic job.
-        :param till:
+
         :param interval: A quantity of a certain time unit
         :return: An unconfigured :class:`Job <Job>`
         """
