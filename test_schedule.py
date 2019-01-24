0f74efc99b503015a155ba5951edbbf4875fc8c0
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-01-23 21:32:18 -0500

    Add tests

--- test_schedule.py
+++ test_schedule.py
@@ -158,12 +158,20 @@ class SchedulerTests(unittest.TestCase):
         assert every().day.at('10:30').do(mock_job).next_run.minute == 30
         assert every().day.at('10:30:50').do(mock_job).next_run.second == 50
 
+        assert every().day.at('8:30').do(mock_job).next_run.hour == 8
+        assert every().day.at('8:30:50').do(mock_job).next_run.hour == 8
+        assert every().day.at('08:30').do(mock_job).next_run.hour == 8
+        assert every().day.at('08:30:50').do(mock_job).next_run.hour == 8
+
         self.assertRaises(ScheduleValueError, every().day.at, '2:30:000001')
         self.assertRaises(ScheduleValueError, every().day.at, '::2')
         self.assertRaises(ScheduleValueError, every().day.at, '.2')
         self.assertRaises(ScheduleValueError, every().day.at, '2')
         self.assertRaises(ScheduleValueError, every().day.at, ':2')
         self.assertRaises(ScheduleValueError, every().day.at, ' 2:30:00')
+        self.assertRaises(ScheduleValueError, every().day.at, '25:30:00')
+        self.assertRaises(ScheduleValueError, every().day.at, '25:30')
+        self.assertRaises(ScheduleValueError, every().day.at, '25')
         self.assertRaises(ScheduleValueError, every().do, lambda: 0)
         self.assertRaises(TypeError, every().day.at, 2)
 
