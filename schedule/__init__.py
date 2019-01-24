b2b2d0bbeb36b105ac29708f07d3b5104511f92a
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-01-23 21:32:10 -0500

    Update regex to make it clearer
    Functionally the same, but clearer that seconds are the optional value

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -394,9 +394,9 @@ class Job(object):
         if not isinstance(time_str, str):
             raise TypeError('at() should be passed a string')
         if self.unit == 'days' or self.start_day:
-            if not re.match(r'^([0-2]?\d:)?[0-5]\d:[0-5]\d$', time_str):
+            if not re.match(r'^[0-2]?\d:[0-5]\d(:[0-5]\d)?$', time_str):
                 raise ScheduleValueError(('Invalid time format '
-                                          '(valid format is (H?H:)?MM:SS)'))
+                                          '(valid format is H?H:MM(:SS)?)'))
         if self.unit == 'hours':
             if not re.match(r'^([0-5]\d)?:[0-5]\d$', time_str):
                 raise ScheduleValueError(('Invalid time format for '
