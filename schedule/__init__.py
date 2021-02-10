6695bc781c0ee41581e3292988cbcfdd0ed0c461
sijmenhuizenga <sijmenhuizenga@gmail.com>
2021-02-10 23:44:54 +0100

    Fix formatting

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -506,7 +506,8 @@ class Job(object):
         self._schedule_next_run()
         if self.scheduler is None:
             raise ScheduleError(
-                "Unable to a add job to schedule. Job is not associated with a scheduler."
+                "Unable to a add job to schedule. "
+                "Job is not associated with a scheduler."
             )
         self.scheduler.jobs.append(self)
         return self
