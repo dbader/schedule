ff95c8106ed9fd4cd8c9d7253444f7ea6bcb570f
Bibin Varghese <bibinvargheset@gmail.com>
2023-02-28 12:39:39 +0530

    next_run_time

--- schedule_precise_example.py
+++ schedule_precise_example.py
@@ -6,21 +6,25 @@ import datetime as dt
 
 def greet(name):
     print('\n Hello', name, dt.datetime.now())
+    pause.seconds(1)
 
 
-schedule.every(2).seconds.do(greet, name='Alice')
-schedule.every(4).seconds.do(greet, name='Bob')
-
+# schedule.every(2).seconds.do(greet, name='Alice')
+# schedule.every(4).seconds.do(greet, name='Bob')
+#
 from schedule import every, repeat
 
+scheduler1 = schedule.Scheduler(start_time_based=False)
 
-@repeat(every().second, "World")
+
+@repeat(scheduler1.every(5).seconds, "World")
 @repeat(every().day, "Mars")
 def hello(planet):
     print("\n Hello1", planet, dt.datetime.now())
+    pause.seconds(2)
 
 
 while True:
-    schedule.run_pending()
+    scheduler1.run_pending()
     next_time = dt.datetime.now().replace(microsecond=0) + dt.timedelta(seconds=1)
     pause.until(next_time)
