f886e2c912eaf8d2df3610bac56a482450144cd6
Andrey Prokhorov <31391185+AnezeR@users.noreply.github.com>
2023-04-28 00:39:19 +0300

    A bit more clear phrasing

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -758,7 +758,7 @@ class Job:
                     .replace(tzinfo=None)
                 )
                 
-                # Changing at_time so checks with it stay consistent.
+                # Applying timezone to at_time as well so that checks with it stay consistent.
                 # Datetime is needed for using pytz library.
                 at_time_datetime = datetime.datetime.combine(
                     datetime.date(2000, 1, 1),  # Arbitrary date
