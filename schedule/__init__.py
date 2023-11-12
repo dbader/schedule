b719314c8e676b7e1a314bba2de494a46538e6a6
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2023-11-12 16:42:47 +0100

    Fix formatting, again

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -810,7 +810,9 @@ class Job:
             microsecond=input.microsecond,
         )
 
-    def _to_at_timezone(self, input: Optional[datetime.datetime]) -> Optional[datetime.datetime]:
+    def _to_at_timezone(
+        self, input: Optional[datetime.datetime]
+    ) -> Optional[datetime.datetime]:
         if self.at_time_zone is None or input is None:
             return input
         return input.astimezone(self.at_time_zone)
