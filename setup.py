1173f2820b6e996fd80c51e61353a31a4c260b3b
Sijmen <sijmenhuizenga@gmail.com>
2023-12-10 10:31:47 +0100

    Add support for 3.12 (#606)

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Natural Language :: English",
     ],
     python_requires=">=3.7",
